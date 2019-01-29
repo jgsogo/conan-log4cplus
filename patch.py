b238bfd65a86319240170e86af8e792ad8025caa
Javier G. Sogo <jgsogo@gmail.com>
2019-01-29 16:37:11 +0100

    spaces

--- patch.py
+++ patch.py
@@ -4,8 +4,15 @@ from conans.tools import replace_in_file
 def main():
     f = r'/Users/travis/.pyenv/versions/conan/lib/python3.4/site-packages/conans/model/manifest.py'
 
-    replace_in_file(f, "the_time = int(tokens[0])", """
-    print("*"*20)\nprint(tokens)\nprint("*"*20)""")
+    replace_in_file(f, "the_time = int(tokens[0])", """print("*"*20)
+        print(tokens)
+        print("*"*20)""")
+
+    f = r'/Users/travis/.pyenv/versions/conan/lib/python3.4/site-packages/conans/model/manifest.py'
+    replace_in_file(f, "# Unroll generator and decode shas (plain text)", 
+                    """print(urls)
+        print(contents.keys())
+        print(contents)""")
 
 
 main()
